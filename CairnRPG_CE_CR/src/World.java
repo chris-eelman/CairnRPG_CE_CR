d927e2040984dc394d119f4c84efadd80d394aed
Cabe22 <125468641+Cabe22@users.noreply.github.com>
2023-03-07 15:53:20 -0800

    Updated World class

--- CairnRPG_CE_CR/src/World.java
+++ CairnRPG_CE_CR/src/World.java
@@ -1,5 +1,29 @@
 public class World {
     public static void main(String[] args) {
+        World gameName = new World("something", "11 as 1");
 
+    }
+    String gameName;
+    String publisher;
+
+    public World(String gameName, String publisher) {
+        this.gameName = gameName;
+        this.publisher = publisher;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
     }
 }
