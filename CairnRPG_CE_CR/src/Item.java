6fe8beff5961e976bf40c84d25fd48f207639f6d
chris-eelman <chriseelman56@gmail.com>
2023-03-07 18:56:10 -0500

    Made Item class constructors, getters/setters and Info method

--- CairnRPG_CE_CR/src/Item.java
+++ CairnRPG_CE_CR/src/Item.java
@@ -1,3 +1,32 @@
 public class Item {
+    private String name;
+    private int healingPower;
+
+    //Constructor
+    public Item(String name, int healingPower) {
+        this.name = name;
+        this.healingPower = healingPower;
+    }
+
+    //Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealingPower() {
+        return healingPower;
+    }
+
+    public void setHealingPower(int healingPower) {
+        this.healingPower = healingPower;
+    }
+
+    public String Info(){
+        return "Item name: " + name + "Healing Power: " + healingPower;
+    }
 
 }
