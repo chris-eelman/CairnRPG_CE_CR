aa5ceb4d3dfa87e589ca1638e4bf37ba8e25eef7
Cabe22 <125468641+Cabe22@users.noreply.github.com>
2023-03-07 20:27:24 -0800

    Updated ItemTest

--- CairnRPG_CE_CR/src/Enemy.java
+++ CairnRPG_CE_CR/src/Enemy.java
@@ -1,15 +1,15 @@
-//public class Enemy {
-//    public class Enemy extends Character{
-//
-//        private int magicPower;
-//
-//
-//    public Enemy (float health, float attackPower, boolean isAlive) {
-//        super("Stormtrooper",health, attackPower, isAlive);
-//        this.magicPower = 30;
-//    }
-//
-//    public Enemy (String name, float health, float attackPower, boolean isAlive) {
-//        super(name, health, attackPower, isAlive);
-//    }
-//}
+public class Enemy {
+    public class Enemy extends Character{
+
+        private int magicPower;
+
+
+    public Enemy (float health, float attackPower, boolean isAlive) {
+        super("Stormtrooper",health, attackPower, isAlive);
+        this.magicPower = 30;
+    }
+
+    public Enemy (String name, float health, float attackPower, boolean isAlive) {
+        super(name, health, attackPower, isAlive);
+    }
+}
