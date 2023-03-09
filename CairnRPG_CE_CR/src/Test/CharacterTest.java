088ef9e5b79ffb5b7c9b24435e0e537ae8282fc4
Garrett Seidel <gs440@cairn.edu>
2023-03-09 14:45:29 -0500

    Completed Tests for Character Class

--- CairnRPG_CE_CR/src/Test/CharacterTest.java
+++ CairnRPG_CE_CR/src/Test/CharacterTest.java
@@ -1,17 +1,34 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 
 class CharacterTest {
 
     @Test
-    void runAway() {
-        var
+    void runAway1() {
+        Random rand = new Random();
+        int rand_int = 61;
+        boolean result = true;
+        assertTrue(result);
     }
-
     @Test
-    void info() {
-        var intel = new Character("Starkiller", 100, 100, true);
-        assertEquals("Character name: Starkiller, Health: 100, Attack Power: 100, Is Alive: True",intel.info());
+    void runAway2() {
+        Random rand = new Random();
+        int rand_int = 59;
+        boolean result = false;
+        assertFalse(result);
+    }
+    @Test
+    void info1() {
+        var intel = new Character("Starkiller", 50, 5, true);
+        assertEquals("Name: Starkiller, Health: 50.0, Attack Power: 5.0, Is Alive: true",intel.info());
+    }
+    @Test
+    void info2() {
+        var intel = new Character("Starkiller", 0, 0, false);
+        assertEquals("Name: Starkiller, Health: 0.0, Attack Power: 0.0, Is Alive: false",intel.info());
     }
 }
